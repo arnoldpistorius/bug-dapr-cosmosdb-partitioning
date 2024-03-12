5c65b9a5a9c34c6c4b88e3ce2c43e911261c0bb4
Arnold Pistorius <info@arnoldpistorius.com>
2024-03-12 15:51:07 +0000

    Initial version of demo application

---
+++ src/Application/MyActorWithReminder.cs
@@ -0,0 +1,43 @@
+﻿using Dapr.Actors;
+using Dapr.Actors.Runtime;
+
+namespace Application;
+
+public class MyActorWithReminder : Actor, IMyActorWithReminder
+{
+    private readonly ILogger<MyActorWithReminder> logger;
+
+    public MyActorWithReminder(ActorHost host, ILogger<MyActorWithReminder> logger) : base(host)
+    {
+        this.logger = logger;
+    }
+
+    public async Task InitializeActor()
+    {
+        using var scope = CreateLoggingScope();
+
+        logger.LogDebug("Registering reminder...");
+        await RegisterReminderAsync("some-reminder-name", null, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(10));
+        logger.LogDebug("Reminder registered.");
+    }
+
+    public async Task StopActor()
+    {
+        using var scope = CreateLoggingScope();
+
+        logger.LogDebug("Unregistering reminder...");
+        await UnregisterReminderAsync("some-reminder-name");
+        logger.LogDebug("Reminder unregistered.");
+    }
+
+    IDisposable? CreateLoggingScope() {
+        return logger.BeginScope("ActorId: {actorId}", Id.GetId());
+    }
+}
+
+public interface IMyActorWithReminder : IActor
+{
+    Task InitializeActor();
+
+    Task StopActor();
+}
---
+++ src/Application/Program.cs
@@ -0,0 +1,43 @@
+﻿using Application;
+using Dapr.Actors;
+using Dapr.Actors.Client;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddLogging(x => x.AddConsole());
+services.AddDaprSidekick(configure => {
+    configure.Sidecar = new() {
+        AppId = "demo",
+        ResourcesDirectory = "dapr-components",
+        DaprHttpPort = 3500,
+        DaprGrpcPort = 50001
+    };
+});
+
+services.AddActors(configure => {
+    configure.RemindersStoragePartitions = 3;
+
+    configure.Actors.RegisterActor<MyActorWithReminder>(nameof(MyActorWithReminder));
+});
+services.AddDaprClient();
+services.AddTransient<ActorProxyFactory>();
+
+var app = builder.Build();
+
+app.MapGet("/{id}/start", async (string id, [FromServices] ActorProxyFactory actorFactory) => {
+    var actor = actorFactory.CreateActorProxy<IMyActorWithReminder>(new ActorId(id), nameof(MyActorWithReminder));
+    await actor.InitializeActor();
+    return "Actor initialized";
+});
+
+app.MapGet("/{id}/stop", async (string id, [FromServices] ActorProxyFactory actorFactory) => {
+    var actor = actorFactory.CreateActorProxy<IMyActorWithReminder>(new ActorId(id), nameof(MyActorWithReminder));
+    await actor.StopActor();
+    return "Actor stopped";
+});
+
+app.MapActorsHandlers();
+
+await app.RunAsync();
